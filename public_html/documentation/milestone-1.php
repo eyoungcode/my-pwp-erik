fb90435ed0b9ed61703e006093e3275f4f3ea17e
Erik Young <youngblkraven@gmail.com>
2019-04-15 15:18:14 -0600

    stated creating persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+Purpose: This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
+
+Audience: We will be targeting Mortgage Brokers and Real Estate Agents.
+
+Goal: The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
+		The second reason for the website will be to showcase positive testimonials from prior clients to help generate new clients.
+
+
+Persona:
+Name: Judy Smith
+Age: 45
+Professional: She has been a Mortgage Broker for the last 10 years. As summer approaches, she will become busier with brokering new house sales
+and refinancing houses. She is looking to increase the amount of appraisers she has working for her to help increase revenue.
+Technology:
+Behaviors and attitudes:
+Situation:
+Frustration:
