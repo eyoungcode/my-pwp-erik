3d99e5ac5dfc3091e24ce721f5218e9f519ad5dd
Erik Young <youngblkraven@gmail.com>
2019-04-17 14:25:18 -0600

    modified it into html format

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,60 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 	<meta charset="UTF-8">
-<p>
-	<strong>Purpose:</strong> This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
-</p>
-<p>
-	<strong>Audience:</strong> We will be targeting Mortgage Brokers and Real Estate Agents.</p>
-<p>
-	<strong>Goal:</strong>
-	The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
+	<p>
+		<strong>Purpose:</strong> This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
+	</p>
+	<p>
+		<strong>Audience:</strong> We will be targeting Mortgage Brokers and Real Estate Agents.</p>
+	<p>
+		<strong>Goal:</strong>
+		The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
 		The second reason for the website will be to showcase positive testimonials from prior clients to help generate new clients.
 	</p>
-<p>
-	<ul>
-	<li>
-		<strong>Persona:</strong>
-</li>
-	<li>
-		<strong>Goal:</strong>
-		To find a Appraiser with a bachelors degree and is license to work in New Mexico. They also need to be reachable during
-nights and weekends for appraiser status.
-</li>
-<li>
-	<strong>Situation:</strong>
-	As summer approaches, she will become busier with brokering new house sales and refinancing houses.
-She is doing a web search looking to increase the amount of appraisers she has working for her to help increase revenue.
-	</li>
-</ul>
-<ul>
-	<li>
-		<strong>Name</strong>
-		Judy Smith
-	</li>
-	<li>
-		<strong>Age</strong>
-		45
-	</li>
-	<li>
-		<strong>Gender</strong>
-		Female
-	</li>
-	<li>
-		<strong>Technical Abilities</strong>
-		Cell phone (uses for both work and personal) - Iphone XS Max, laptop - Apple MacBook Air.
-		Doesn't use a tablet because she can accomplish her work on her laptop and smartphone.
-	</li>
-		<li>
-		<strong>Occupation</strong>
+	<p>
+			<strong>Persona:</strong>
+	</p>
+
+	<p>
+			<strong>Goal:</strong>
+			To find a Appraiser with a bachelors degree and is license to work in New Mexico. They also need to be reachable during
+			nights and weekends for appraiser status.
+		</p>
+		<p>
+			<strong>Situation:</strong>
+			As summer approaches, she will become busier with brokering new house sales and refinancing houses.
+			She is doing a web search looking to increase the amount of appraisers so can increase revenue.
+		</p>
+
+		<p>
+			<strong>Name</strong>
+			Judy Smith
+		</p>
+		<p>
+			<strong>Age</strong>
+			45
+		</p>
+		<p>
+			<strong>Gender</strong>
+			Female
+		</p>
+		<p>
+			<strong>Technical Abilities</strong>
+			Cell phone (uses for both work and personal) - Iphone XS Max, laptop - Apple MacBook Air.
+			Doesn't use a tablet because she can accomplish her work on her laptop and smartphone.
+		</p>
+		<p>
+			<strong>Occupation</strong>
 			She is a bilingual Mortgage Broker for the last 10 years with Indigo Mortgage.
-	</li>
-	<li>
-		<strong>Attributes</strong>
-		<p>Very business oriented</p>
-		<p>Time=money</p>
-		<p>Will work nights and weekends to close a deal</p>
-		<p>Doesn't like it when people are slow and/or dishonest with what they are trying to accomplish</p>
-	</li>
-</ul>
+		</p>
+
+
+			<strong>Attributes</strong>
+	<ul>
+			<li>Very business oriented</li>
+			<li>Time = money</li>
+			<li>Will work nights and weekends to close a deal</li>
+			<li>Doesn't like it when people are slow and/or dishonest with what they are trying to accomplish</li>
+
+	</ul>
 </html>
