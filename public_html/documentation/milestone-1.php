4c65be9d385bff37b2c5badaf44d69fc82669945
Erik Young <youngblkraven@gmail.com>
2019-04-17 13:15:09 -0600

    modify coding into html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,5 +1,9 @@
-Purpose: This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
-
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="UTF-8">
+<p>
+	<strong>Purpose:</strong> This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
+</p>
 Audience: We will be targeting Mortgage Brokers and Real Estate Agents.
 
 Goal: The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
@@ -7,21 +11,37 @@ Goal: The goal is to create a professional and easy to use website for Mortgage 
 
 
 Persona:
-Name: Judy Smith
-Age: 45
-
-Professional: She is a bilingual Mortgage Broker for the last 10 years with Indigo Mortgage.
-
-Technology: Cell phone (uses for both work and personal) - Iphone XS Max, laptop - Apple MacBook Air, doesn't use a tablet because she can accomplish her work on her laptop and smartphone.
-
-Attitudes:
-Very business orientated
-Time = money
-Will work nights and weekends to close a deal
-Doesn't like it when people are slow and/or dishonest with what they are trying to accomplish
-
 Goal: To find a Appraiser with a bachelors degree and is license to work in New Mexico. They also need to be reachable during
 nights and weekends for appraiser status.
 
 Situation:  As summer approaches, she will become busier with brokering new house sales and refinancing houses.
 She is doing a web search looking to increase the amount of appraisers she has working for her to help increase revenue.
+<ul>
+	<li>
+		<strong>Name</strong>
+		Judy Smith
+	</li>
+	<li>
+		<strong>Age</strong>
+		45
+	</li>
+	<li>
+		<strong>Gender</strong>
+		Female
+	</li>
+	<li>
+		<strong>Technical Abilities</strong>
+		Cell phone (uses for both work and personal) - Iphone XS Max, laptop - Apple MacBook Air.
+		Doesn't use a tablet because she can accomplish her work on her laptop and smartphone.
+	</li>
+		<li>
+		<strong>Occupation</strong>
+			She is a bilingual Mortgage Broker for the last 10 years with Indigo Mortgage.
+	</li>
+	<li>
+		<strong>Attributes</strong>
+		<p>Very business oriented</p>
+		<p>Time=money</p>
+		<p>Will work nights and weekends to close a deal</p>
+		<p>Doesn't like it when people are slow and/or dishonest with what they are trying to accomplish</p>
+	</li>
