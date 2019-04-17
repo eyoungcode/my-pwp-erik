211c13a5b092118022a12e1deb4a5f9a6e05e395
Erik Young <youngblkraven@gmail.com>
2019-04-17 13:50:03 -0600

    modify coding into html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
-	<meta charset="UTF-8">
 <p>
 	<strong>Purpose:</strong> This project will be to focus on create a professional website for my friend who is a Real Estate Appraiser.
 </p>
-Audience: We will be targeting Mortgage Brokers and Real Estate Agents.
-
-Goal: The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
+<p>
+	<strong>Audience:</strong> We will be targeting Mortgage Brokers and Real Estate Agents.</p>
+<p>
+	<strong>Goal:</strong>
+	The goal is to create a professional and easy to use website for Mortgage Brokers and Real Estate Agents to contact him.
 		The second reason for the website will be to showcase positive testimonials from prior clients to help generate new clients.
+	</p>
 
-
-Persona:
-Goal: To find a Appraiser with a bachelors degree and is license to work in New Mexico. They also need to be reachable during
+<p>
+	<ul>
+	<li>
+		<strong>Persona:</strong>
+</li>
+	<li>
+		<strong>Goal:</strong>
+		To find a Appraiser with a bachelors degree and is license to work in New Mexico. They also need to be reachable during
 nights and weekends for appraiser status.
-
-Situation:  As summer approaches, she will become busier with brokering new house sales and refinancing houses.
+</li>
+<li>
+	<strong>Situation:</strong>
+	As summer approaches, she will become busier with brokering new house sales and refinancing houses.
 She is doing a web search looking to increase the amount of appraisers she has working for her to help increase revenue.
+	</li>
+</ul>
 <ul>
 	<li>
 		<strong>Name</strong>
@@ -45,3 +55,4 @@ She is doing a web search looking to increase the amount of appraisers she has w
 		<p>Will work nights and weekends to close a deal</p>
 		<p>Doesn't like it when people are slow and/or dishonest with what they are trying to accomplish</p>
 	</li>
+</ul>
