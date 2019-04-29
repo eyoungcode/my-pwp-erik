0e82afd7393cf7342828ae6563e99231e905bcbf
Erik Young <youngblkraven@gmail.com>
2019-04-28 20:13:48 -0600

    added welcome pagae

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,12 @@
+<p>
+	<strong>Welcome:</strong>This will identify my friend by name and why they should do business with him.
+</p>
+<p>
+	<strong>About Me:</strong>This section will include his work history and licenses.
+</p>
+<p>
+	<strong>Testimonial:</strong> Positive feedback from his clients 3 - 5 total testimonials.
+</p>
+<p>
+	<strong>Contact info:</strong> This section will include my friends professional email address, I will help him create his Linkedin profile and add the link to his page.
+</p>
